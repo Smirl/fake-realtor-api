87eb51bb2db969d4cbac63633c14de3e59de07ae
Alex Williams <alex.williams@skyscanner.net>
2015-10-27 14:20:31 +0000

    Add the flask app, requirements, and templates

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# fake-realtor-api
-An example data source to be used with the Tolomuco framework.
+# Tolomuco Real Estate API
+## An example data source to be used with the Tolomuco framework.
+
+### Usage
+
+The API is available at /api/ and can take the following GET arguments:
+
+
+GET parameter | Default        | Description
+--------------|----------------|-----------------------------------
+per_page      |10              | the number of results per page
+page          | 1              | the page number. First page is 1.
+total         | `per_page * 2` | the number of results in total
+seed          | 1337405335     | the random number genorator seed.
---
+++ app.py
@@ -0,0 +1,77 @@
+"""A simple flask app to return random Real Estate properties."""
+
+from __future__ import division
+from math import ceil
+
+from flask import Flask, request, jsonify, render_template
+from faker import Factory
+
+app = Flask(__name__)
+
+
+def random_cost(faker, min_order=5, max_order=6):
+    """Return a random money amount in the correct order of magintude."""
+    order = faker.random_int(min_order, max_order)
+    return float(''.join([
+        str(faker.random_number(order)),
+        '.',
+        str(faker.random_number(2))
+    ]))
+
+
+def random_property(faker):
+    """
+    Return a dict which represets a property.
+
+    The order of these is important to keep the random seed the same. When
+    adding new fields the data will change.
+    """
+    return dict([
+        ('property_id', faker.sha1()),
+        ('street_address', faker.street_address()),
+        ('city', faker.city()),
+        ('country', faker.country()),
+        ('post_code', faker.postcode()),
+        ('latitude', float(faker.latitude())),
+        ('longitude', float(faker.longitude())),
+        ('bedrooms', faker.random_int(min=1, max=5)),
+        ('cost', random_cost(faker)),
+        ('deposit', random_cost(faker, 2, 4)),
+    ])
+
+
+@app.route('/')
+def home():
+    """A simple HTML help page to the API."""
+    return render_template('index.html')
+
+
+@app.route('/api/')
+def api():
+    """The only route. Return predictable but random Real Estate properties."""
+    per_page = request.args.get('per_page', 10, int)
+    page = request.args.get('page', 1, int) - 1  # 0 based pages in code.
+    total = request.args.get('total', per_page * 2, int)
+    seed = request.args.get('seed', 1337405335, int)
+    skipped = per_page * page
+
+    faker = Factory.create()
+    faker.seed(seed)
+
+    properties = []
+    for x in range(total):
+        prop = random_property(faker)
+        if skipped <= x < min(skipped + per_page, total):
+            properties.append(prop)
+
+    return jsonify({
+        'properties': properties,
+        'per_page': per_page,
+        'page': page + 1,
+        'total': total,
+        'total_pages': ceil(total / per_page),
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
